3e8e1727884c09d0e3869d184bdaf9140579e12b
Teppei Akada <teppei.akada@sidefeed.com>
2022-02-20 00:31:44 +0900

    Create the project

---
+++ AnimationMaker/AnimationMaker/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  AnimationMaker
+//
+//  Created by AKADA TEPPEI on 2022/02/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = ViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
---
+++ AnimationMaker/AnimationMaker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AnimationMaker
+//
+//  Created by AKADA TEPPEI on 2022/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
